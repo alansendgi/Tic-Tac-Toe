b179542d659feac986e9c9c42e5c0742d324aef7
Alan Sendgikoski <asendgi@gmail.com>
2013-07-15 14:09:38 -0500

    created setup.py, initialized database

---
+++ Tic_Tac_Toe/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Tic_Tac_Toe',
+    version='0.1',
+    description='Django Tic-Tac-Toe',
+    author='Alan Sendgikoski',
+    author_email='asendgi@gmail.com',
+    install_requires=[
+        'Django==1.5.1',
+    ]
+)
