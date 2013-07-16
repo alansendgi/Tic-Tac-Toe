43786c775fd19e66af7706bc4d473671d1b7d76e
Alan Sendgikoski <asendgi@gmail.com>
2013-07-16 12:45:53 -0500

    changes to models, views, urls

--- game/admin.py
+++ game/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from game.models import Player
+from game.models import Game
 from game.models import Move
 
-admin.site.register(Player)
+admin.site.register(Game)
 admin.site.register(Move)
--- game/models.py
+++ game/models.py
@@ -1,19 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User
-import datetime
-from django.utils import timezone
+import random
 
-class Player(models.Model):
-    accepted = models.BooleanField()
-    rejected = models.BooleanField()
-    created = models.DateTimeField(auto_now_add=True)
+class Game(models.Model):
+    # Define Game class
+    pass
+
+    # I think these go to views.py
+    # sets up game board, and assigns game piece.
+    def drawBoard(board):
+        pass
     
+    def getPlayerLetter():
+        # Let the human choose which letter they want to be.
+        # Returns a list with the player's letter as the first item, 
+        # and the computer's letter as the second.
+        letter = ''
+        while not (letter == 'X' or letter == 'O'):
+            print('Do you want to be X or O?')
+            letter = input().upper()
+        
     
 class Move(models.Model):
-    player = models.ForeignKey(Player)
+    # define the Move class to track game playing
     move = models.CharField(max_length=255)
-    selectXY = models.PositiveSmallIntegerField()
-    created = models.DateTimeField(auto_now_add=True)
+    select_square = models.PositiveSmallIntegerField()
     
     def __unicode__(self):
-        return self.move
+        return self.move, self.select_square
