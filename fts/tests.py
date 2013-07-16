79c4a451532358e12167e9f2b4afb3af94540b5a
Alan Sendgikoski <asendgi@gmail.com>
2013-07-16 11:30:07 -0500

    updated functional test app

--- fts/tests.py
+++ fts/tests.py
@@ -4,8 +4,10 @@ http://www.tdd-django-tutorial.com/tutorial/1/
 """
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 class AdminTest(LiveServerTestCase):
+    fixtures = ['admin_user.json']
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -22,5 +24,22 @@ class AdminTest(LiveServerTestCase):
         body = self.browser.find_element_by_tag_name('body')
         self.assertIn('Django administration', body.text)
 
-        # TODO: this needs work
-        self.fail('finish this test')
+        # She types in her username and passwords and hits return
+        username_field = self.browser.find_element_by_name('username')
+        username_field.send_keys('admin')
+
+        password_field = self.browser.find_element_by_name('password')
+        password_field.send_keys('admin')
+        password_field.send_keys(Keys.RETURN)
+
+        # her username and password are accepted, and she is taken to
+        # the Site Administration page
+        body = self.browser.find_element_by_tag_name('body')
+        self.assertIn('Site administration', body.text)
+
+        # She now sees a couple of hyperlink that says "Polls"
+        polls_links = self.browser.find_elements_by_link_text('Polls')
+        self.assertEquals(len(polls_links), 2)
+
+        # TODO: Gertrude uses the admin site to create a new Poll
+        self.fail('todo: finish tests')
